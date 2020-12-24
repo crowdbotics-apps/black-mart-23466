4e9ce9590c6ca0b4d95ddee9f3605fbe644ecb77
Crowdbotics <team@crowdbotics.com>
2020-12-24 19:39:57 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen64187990', human_name: 'Blank Screen 64', access_route: 'BlankScreen64187990'},
 { name: 'BlankScreen63187989', human_name: 'Blank Screen 63', access_route: 'BlankScreen63187989'},
 { name: 'CalendarView52186784', human_name: 'Calendar View 52', access_route: 'CalendarView52186784', icon: 'calendar'},
 { name: 'CalendarView53186783', human_name: 'Calendar View 53', access_route: 'CalendarView53186783', icon: 'calendar'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen64187990Navigator from '../features/BlankScreen64187990/navigator';
 import BlankScreen63187989Navigator from '../features/BlankScreen63187989/navigator';
 import CalendarView52186784Navigator from '../features/CalendarView52186784/navigator';
 import CalendarView53186783Navigator from '../features/CalendarView53186783/navigator';
@@ -30,6 +31,7 @@ import BlankScreen0186723Navigator from '../features/BlankScreen0186723/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen64187990: { screen: BlankScreen64187990Navigator },
 BlankScreen63187989: { screen: BlankScreen63187989Navigator },
 CalendarView52186784: { screen: CalendarView52186784Navigator },
 CalendarView53186783: { screen: CalendarView53186783Navigator },
